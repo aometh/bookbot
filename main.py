1507f2fa0cce2d4062f16b080008fee6d8a5f5b8
aometh <edwin.orellana92@gmail.com>
2024-11-07 12:37:05 +1100

    Adding Get Character Count function

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    character_count = get_character_count(text)
     num_words = count(text)
-    print(f"{num_words} words found in the document")
+    return print(f"{character_count}")
+
+
+    #print(f"{num_words} words found in the document")
 
 def count(text):
     words = text.split() 
     return len(words)
 
 def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+   with open(path) as f:
+      return f.read()
+
+def get_character_count(text):
+    text = text.lower()
+    small_text = text.split()
+    character_lib = {}
+    for word in small_text:
+        for c in word:
+            if c in character_lib:
+                character_lib[c] += 1
+            else:
+                character_lib.update({c:1})
+    return character_lib
+
 
 main()
